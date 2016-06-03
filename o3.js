779756656ffb1d7c96d781aefea45057bc5d75a0
OZv <voc_bt@yahoo.co.jp>
2016-06-03 21:06:39 +0800

    Update o3.js

--- o3.js
+++ o3.js
@@ -1,22 +1,20 @@
-o0e=1;
-function xh5(c,d){
-var n=c.parentNode.nextSibling;
-if(d)n=n.childNodes[0];
+var o0e=(function(){return{
+e:function(c,d){
+var n=d==2?c.nextSibling:c.parentNode.nextSibling;
+if(!d)n=n.childNodes[0];
 with(n.style)
 if(display!="block")display="block";
 else display="none";
-}
-function atv(c,d,f){
+},
+a:function(c,d,f){
 c.removeAttribute("onclick");
 with(c.style){
 cursor="default";
 outline="1px dotted gray";
 }
-var u="http://www.oxforddictionaries.com/media/";
-if(d==1)u+="american_english/us_pron/";
-else u+="english/uk_pron/";
-u+=f+".mp3";
-var b=function(){with(c.style){outline="";cursor="pointer";}c.setAttribute("onclick","atv(this,"+d+",'"+f+"')");};
+var r=["english/uk_pron/","english/us_pron/","american_english/us_pron/"];
+var u="http://www.oxforddictionaries.com/media/"+r[d]+f+".mp3";
+var b=function(){with(c.style){outline="";cursor="pointer";}c.setAttribute("onclick","o0e.a(this,"+d+",'"+f+"')");};
 var t=setTimeout(b,2000);
 try{
 with(document.createElement("audio")){
@@ -28,4 +26,21 @@ play();
 }catch(e){
 c.style.outline="";
 }
+},
+x:function(c){
+var n=c.parentNode.nextSibling;
+with(n.style)
+if(display!="none"){
+display="none";
+c.className="yuq";
+}else{
+display="block";
+c.className="aej";
 }
+},
+p:function(c){
+if(c.className=="j02")
+c.className="g4p";
+else c.className="j02";
+}
+}}());
